e1e4601b5452dcb67e12db495aaf2f1fa72ff6bc
orangeola <orangeqmm@gmail.com>
2022-05-07 04:16:30 +0930

    Add function to display results. Add answer sanitisation in the game function

--- script.js
+++ script.js
@@ -39,10 +39,20 @@ function playRound(playerSelection, computerSelection)
     }   
 }
 
+function results(playerWin, compWin){
+    if(playerWin > compWin){
+        console.log("You win! " + playerWin + " against " + compWin);
+    } else if(compWin > playerWin){
+        console.log("You lose... " + playerWin + " against " + compWin);
+    } else {
+        console.log("Tie! " + playerWin + " against " + compWin);
+    }
+}
+
 function game(){
     let playerWin = 0;
     let compWin = 0;
-    
+
     for (let i = 0; i < 5; i++) {
         let userChoice = prompt("Enter move");
 
@@ -51,25 +61,24 @@ function game(){
             break;
         }
 
+        console.log(userChoice);
+        if(userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors"){
+            console.log("Invalid input.");
+            i--;
+        }
+
         userChoice = userChoice.toLowerCase();
         let answer = playRound(userChoice, computerPlay());
 
         console.log(answer);
         if(answer.includes("win")){
             playerWin++;
-        } else {
+        } else if(answer.includes("lose")){
             compWin++;
         }
      }
-
-     if(playerWin > compWin){
-         console.log("You win! " + playerWin + " against " + compWin);
-     } else if(compWin > playerWin){
-        console.log("You lose... " + playerWin + " against " + compWin);
-     } else {
-        console.log("Tie! " + playerWin + " against " + compWin);
-     }
+     
+     results(playerWin, compWin);
 }
 
-//console.log(playRound("PAPEr", "ROck"));
 game();
