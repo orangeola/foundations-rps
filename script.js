c71a12fd2e8fdcb3296d2cd56b6507088f0b37db
orangeola <orangeqmm@gmail.com>
2022-05-07 03:44:29 +0930

    Add playround and game functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function computerPlay()
 {
-    const moves = ["Rock", "Paper", "Scissors"];
+    const moves = ["rock", "paper", "scissors"];
     return moves[random()];
 }
 
@@ -9,3 +9,67 @@ function random()
     return Math.floor(Math.random() * (2 - 0 + 1)) + 0;
 }
 
+function playRound(playerSelection, computerSelection)
+{
+    if(playerSelection === computerSelection){
+        return "Tie!";
+    }
+
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection === 'scissors'){
+                return "You win! Rock beats scissors!";
+            } else {
+                return "You lose...";
+            }
+        case 'scissors':
+            if (computerSelection === 'paper'){
+                return "You win! Scissors beats paper!";
+            } else {
+                return "You lose...";
+            }
+        case 'paper':
+            if (computerSelection === 'rock'){
+                return "You win! Paper beats rock!";
+            } else {
+                return "You lose...";
+            }
+        default:    
+        return "Try again...";
+    }   
+}
+
+function game(){
+    let playerWin = 0;
+    let compWin = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        let userChoice = prompt("Enter move");
+
+        if(userChoice === null){
+            console.log("You quit.");
+            break;
+        }
+
+        userChoice = userChoice.toLowerCase();
+        let answer = playRound(userChoice, computerPlay());
+
+        console.log(answer);
+        if(answer.includes("win")){
+            playerWin++;
+        } else {
+            compWin++;
+        }
+     }
+
+     if(playerWin > compWin){
+         console.log("You win! " + playerWin + " against " + compWin);
+     } else if(compWin > playerWin){
+        console.log("You lose... " + playerWin + " against " + compWin);
+     } else {
+        console.log("Tie! " + playerWin + " against " + compWin);
+     }
+}
+
+//console.log(playRound("PAPEr", "ROck"));
+game();
